2b4455ef0008e000894961dd37a143f35fbfdf2c
pallavi002 <pallavilodhi08@gmail.com>
2020-06-23 12:41:38 +0530

    ss

--- app.js
+++ app.js
@@ -95,22 +95,88 @@ app.get('/add', function (res, req, next) {
     sum: function (line, idx) {
       let s = 0;
       line = line.split(',').map(d => +(d.trim()));
-      for (let i = 0; i < line.length; i++) {
-        s += line[i];
-      }
+      s = line[1] + line[2]
       return s;
     }
   });
   res.res.redirect('back');
 })
 
-app.get('/csvfile', async function(req, res) {
-  var img = await Calculation.findById({_id : '5ef0f302f42e484bec762a6b'});
-  // console.log(img.image);
-  res.render('csvfile', {
-    img : img.image
+app.get('/substract', function (res, req, next) {
+  function subCols(path, data) {
+    let csv = fs.readFileSync(path, 'utf8');
+    csv = csv.split('\n').map(line => line.trim());
+    let colNames = Object.keys(data);
+    for (let i = 0; i < colNames.length; i++) {
+      let c = colNames[i];
+      if (typeof data[c] === 'function') {
+        csv = csv.map((line, idx) => idx === 0
+          ? line + ',' + c
+          : line + ',' + data[c](line, idx)
+        );
+      } else if (Array.isArray(data[c])) {
+        csv = csv.map((line, idx) => idx === 0
+          ? line + ',' + c
+          : line + ',' + data[c][idx - 1]
+        );
+      }
+    }
+    fs.createWriteStream(path, {
+      flag: 'w',
+      defaultEncoding: 'utf8'
+    }).end(csv.join('\n'));
+    
+  }
+
+  subCols('./public/products.csv', {
+    Updates_Minus_Salary: function (line, idx) {
+      let s = 0;
+      line = line.split(',').map(d => +(d.trim()));
+      s = line[1]-line[3]
+      return s;
+    }
   });
+  res.res.redirect('back');
 })
+
+app.get('/double', function (res, req, next) {
+  function doubleCols(path, data) {
+    let csv = fs.readFileSync(path, 'utf8');
+    csv = csv.split('\n').map(line => line.trim());
+    let colNames = Object.keys(data);
+    for (let i = 0; i < colNames.length; i++) {
+      let c = colNames[i];
+      if (typeof data[c] === 'function') {
+        csv = csv.map((line, idx) => idx === 0
+          ? line + ',' + c
+          : line + ',' + data[c](line, idx)
+        );
+      } else if (Array.isArray(data[c])) {
+        csv = csv.map((line, idx) => idx === 0
+          ? line + ',' + c
+          : line + ',' + data[c][idx - 1]
+        );
+      }
+    }
+    fs.createWriteStream(path, {
+      flag: 'w',
+      defaultEncoding: 'utf8'
+    }).end(csv.join('\n'));
+    
+  }
+
+  doubleCols('./public/products.csv', {
+    Double_Salary: function (line, idx) {
+      let s = 0;
+      line = line.split(',').map(d => +(d.trim()));
+      s = line[1]*2
+      return s;
+    }
+  });
+  res.res.redirect('back');
+})
+
+
 let port = (process.env.PORT || '5000')
 app.listen(port ,process.env.IP,  function () {
   console.log('Server running on port 5000');
